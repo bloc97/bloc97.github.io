27a4cff63638c559a20447588914c02e21ce3fe8
bloc97 <bloc97@users.noreply.github.com>
2018-12-18 22:02:17 -0500

    moved model

---
+++ azurlane/data/utils.js
@@ -0,0 +1,40 @@
+
+function getUrlParams(search) {
+	let hashes = search.slice(search.indexOf('?') + 1).split('&')
+	let params = {}
+	hashes.map(hash => {
+		let [key, val] = hash.split('=')
+		params[key] = decodeURIComponent(val)
+	})
+
+	return params
+}
+async function getModel(){
+	const model = await tf.loadModel('https://raw.githubusercontent.com/bloc97/bloc97.github.io/master/azurlane/data/model.json');
+	return model;
+}
+
+function render(latent, canvas, scale) {
+	if (scale < 1) {
+		scale = 1;
+	}
+	
+	var image = tf.add(tf.div(tf.reshape(loadedModel.predictOnBatch(tf.reshape(latent, [1, 1, 1, latent.shape[0]])), [48, 48, 3]), 2), 0.5);
+	image = tf.image.resizeNearestNeighbor(image, [48 * Math.floor(scale), 48 * Math.floor(scale)])
+	
+	tf.toPixels(image, canvas)
+}
+
+var loadedModel = false
+
+function show() {
+	if (!loadedModel) {
+		getModel().then(model => {
+			loadedModel = model
+			render(latent, canvas, scale)
+		});
+	} else {
+		render(latent, canvas, scale)
+	}
+
+}
